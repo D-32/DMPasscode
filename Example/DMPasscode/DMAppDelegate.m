0f71ed62ee912aa9e904853a6736a0614febe832
Dylan Marriott <info@d-32.com>
2014-09-21 21:34:22 +0200

    Improved demo, added remove button

--- Example/DMPasscode/DMAppDelegate.m
+++ Example/DMPasscode/DMAppDelegate.m
@@ -11,6 +11,9 @@
 
 @implementation DMAppDelegate {
     UIViewController* _rootViewController;
+    UIButton* _setupButton;
+    UIButton* _checkButton;
+    UIButton* _removeButton;
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
@@ -20,25 +23,36 @@
     _rootViewController = [[UIViewController alloc] init];
     self.window.rootViewController = _rootViewController;
     [self addViews];
+    [self updateViews];
     return YES;
 }
 
 - (void)addViews {
-    UIButton* setup = [[UIButton alloc] initWithFrame:CGRectMake(0, 50, _rootViewController.view.frame.size.width, 50)];
-    [setup setTitle:@"Setup" forState:UIControlStateNormal];
-    [setup setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [setup addTarget:self action:@selector(actionSetup:) forControlEvents:UIControlEventTouchUpInside];
-    [_rootViewController.view addSubview:setup];
+    _setupButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 50, _rootViewController.view.frame.size.width, 50)];
+    [_setupButton setTitle:@"Setup" forState:UIControlStateNormal];
+    [_setupButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [_setupButton addTarget:self action:@selector(actionSetup:) forControlEvents:UIControlEventTouchUpInside];
+    [_rootViewController.view addSubview:_setupButton];
     
-    UIButton* check = [[UIButton alloc] initWithFrame:CGRectMake(0, 100, _rootViewController.view.frame.size.width, 50)];
-    [check setTitle:@"Check" forState:UIControlStateNormal];
-    [check setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [check addTarget:self action:@selector(actionCheck:) forControlEvents:UIControlEventTouchUpInside];
-    [_rootViewController.view addSubview:check];
+    _checkButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 100, _rootViewController.view.frame.size.width, 50)];
+    [_checkButton setTitle:@"Check" forState:UIControlStateNormal];
+    [_checkButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [_checkButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
+    [_checkButton addTarget:self action:@selector(actionCheck:) forControlEvents:UIControlEventTouchUpInside];
+    [_rootViewController.view addSubview:_checkButton];
+    
+    _removeButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 150, _rootViewController.view.frame.size.width, 50)];
+    [_removeButton setTitle:@"Remove" forState:UIControlStateNormal];
+    [_removeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [_removeButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
+    [_removeButton addTarget:self action:@selector(actionRemove:) forControlEvents:UIControlEventTouchUpInside];
+    [_rootViewController.view addSubview:_removeButton];
 }
 
 - (void)actionSetup:(UIButton *)sender {
-    [DMPasscode setupPasscodeInViewController:_rootViewController completion:^(BOOL success) {}];
+    [DMPasscode setupPasscodeInViewController:_rootViewController completion:^(BOOL success) {
+        [self updateViews];
+    }];
 }
 
 - (void)actionCheck:(UIButton *)sender {
@@ -48,7 +62,20 @@
         } else {
             [sender setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
         }
+        [self updateViews];
     }];
+}
+
+- (void)actionRemove:(id)sender {
+    [DMPasscode removePasscode];
+    [_checkButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self updateViews];
+}
+
+- (void)updateViews {
+    BOOL passcodeSet = [DMPasscode isPasscodeSet];
+    _checkButton.enabled = passcodeSet;
+    _removeButton.enabled = passcodeSet;
 }
 
 @end
