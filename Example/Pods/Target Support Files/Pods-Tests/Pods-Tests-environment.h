9cd6611d7c4617d2c5496ef2c085fdb0b0e46efd
Dylan Marriott <info@d-32.com>
2014-12-11 22:42:25 +0100

    New pod version

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -9,7 +9,7 @@
 // DMPasscode
 #define COCOAPODS_POD_AVAILABLE_DMPasscode
 #define COCOAPODS_VERSION_MAJOR_DMPasscode 1
-#define COCOAPODS_VERSION_MINOR_DMPasscode 1
+#define COCOAPODS_VERSION_MINOR_DMPasscode 2
 #define COCOAPODS_VERSION_PATCH_DMPasscode 0
 
 // Expecta
